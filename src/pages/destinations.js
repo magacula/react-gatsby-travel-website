ffbaabcc6a2dadef0221bc81f447b0de23a3eb53
Koi <Makoi.Gacula@student.csulb.edu>
2020-12-02 16:45:14 -0800

    added pages utlizing gatsby

---
+++ src/pages/destinations.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Destinations = () => (
+  <Layout>
+    <SEO title="Page three" />
+    <h1>Destinations</h1>
+  </Layout>
+)
+
+export default Destinations
---
+++ src/pages/images.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Images = () => (
+  <Layout>
+    <SEO title="Page two" />
+    <h1>Images</h1>
+  </Layout>
+)
+
+export default Images
