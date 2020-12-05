923b2932d20c34b30d2bd8d5f042b9509c7d88d8
Koi <Makoi.Gacula@student.csulb.edu>
2020-12-04 22:10:14 -0800

    created Button component

---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+export const Button = styled(Link)`
+  background: ${({ primary }) => (primary ? "#F26A2E" : "#077BF1")};
+  white-space: nowrap;
+  padding: 14px 32px;
+  color: #fff;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  min-width: 100px;
+  text-decoration: none;
+  transition: 0.3s !important;
+  border-radius: 50px;
+  cursor: pointer;
+
+  &:hover {
+    background: ${({ primary }) => (primary ? "#077BF1" : "#F26A2E")};
+    transform: translateY() (-5px);
+  }
+`
