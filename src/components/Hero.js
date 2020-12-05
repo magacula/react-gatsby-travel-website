d04655cdeba8f8744f4a63563c261060536ab372
Koi <Makoi.Gacula@student.csulb.edu>
2020-12-04 23:19:03 -0800

    added Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,101 @@
+import React from "react"
+import styled from "styled-components"
+import { Button } from "./Button"
+import Video from "../assets/bg_video2.mp4"
+
+const Hero = () => {
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg src={Video} type="video/mp4" autoPlay loop muted playsInline />
+      </HeroBg>
+      <HeroContent>
+        <HeroItems>
+          <Hero_h1>Take An Escape</Hero_h1>
+          <Hero_p>Plan your next trip today</Hero_p>
+          <Button primary="true">Lets Get Started</Button>
+        </HeroItems>
+      </HeroContent>
+    </HeroContainer>
+  )
+}
+
+export default Hero
+
+const HeroContainer = styled.div`
+  display: flex;
+  background: #0c0c0c;
+  justify-content: center;
+  align-items: center;
+  width: 100%1;
+  height: 100vh;
+  padding: 0 1rem;
+  position: relative;
+  margin-top: -80px;
+  color: #fff;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    z-index: 2;
+  }
+`
+
+const HeroBg = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`
+
+const VideoBg = styled.video`
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+`
+
+const HeroContent = styled.div`
+  z-index: 3;
+  height: calc(100vh - 80px);
+  max-height: 100%;
+  padding: 0rem calc((100vw - 1300px) / 2);
+`
+
+const HeroItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100vh;
+  max-height: 100%;
+  padding: 0;
+  color: #fff;
+  line-height: 1.2;
+  font-weight: bold;
+`
+
+const Hero_h1 = styled.h1`
+  // clamp can help resize fonts instead of using media queries
+  // clamp (smallest size font, amount to increase, max size font)
+  font-size: clamp(1.8rem, 6vw, 4rem);
+  margin-bottom: 1.5rem;
+  letter-spacing: 3px;
+  padding: 0 1rem;
+  font-family: "PT Sans Caption", sans-serif;
+`
+
+const Hero_p = styled.p`
+  font-size: clamp(1rem, 3vw, 2rem);
+  margin-bottom: 2rem;
+  font-family: "PT Sans Caption", sans-serif;
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Hero from "../components/Hero"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,7 +7,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home Page</h1>
+    <Hero />
   </Layout>
 )
 
