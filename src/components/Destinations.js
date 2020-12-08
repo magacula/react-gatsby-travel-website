6fc8d47fbc4b83631e15b210f667245e1ad8c4bb
Koi <Makoi.Gacula@student.csulb.edu>
2020-12-08 15:56:55 -0800

    added Destinations page

---
+++ src/components/Destinations.js
@@ -0,0 +1,173 @@
+import React from "react"
+import styled from "styled-components"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import { Button } from "./Button"
+import { ImLocation } from "react-icons/im"
+
+/*
+ * This component is built using `gatsby-image` to automatically serve optimized
+ * images with lazy loading and reduced file sizes. The image is loaded using a
+ * `useStaticQuery`, which allows us to load the image from directly within this
+ * component, rather than having to pass the image data down from pages.
+ */
+
+const Destinations = () => {
+  const data = useStaticQuery(graphql`
+    query TripsQuery {
+      allDataJson {
+        edges {
+          node {
+            alt
+            button
+            name
+            img {
+              childImageSharp {
+                fluid {
+                  # copies all additional GatsbyImageSharpFluid props
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  function getData(data) {
+    const tripsArray = []
+
+    // iterating through our GraphQL query to retrieve properties of image
+    // and push it to our array
+    data.allDataJson.edges.forEach((item, index) => {
+      tripsArray.push(
+        <TripCard key={index}>
+          <TripImg
+            src={item.node.img.childImageSharp.fluid.src}
+            fluid={item.node.img.childImageSharp.fluid}
+          />
+          <TripInfo>
+            <TextWrap>
+              <ImLocation />
+              <TripTitle>{item.node.name}</TripTitle>
+            </TextWrap>
+            {/* We can use css property to add an inline style to our custom component */}
+            <Button to="/destinations" primary="true" style={buttonStyle}>
+              {item.node.button}
+            </Button>
+          </TripInfo>
+        </TripCard>
+      )
+    })
+    return tripsArray
+  }
+
+  return (
+    <div>
+      <TripsContainer>
+        <TripsHeading>Featured Destinations</TripsHeading>
+        <TripsWrapper>{getData(data)}</TripsWrapper>
+      </TripsContainer>
+    </div>
+  )
+}
+
+export default Destinations
+
+/***********************************************************************************
+ ****************** Trips Styled Components ****************************************
+ ***********************************************************************************
+ */
+
+const TripsContainer = styled.div`
+  min-height: 100vh;
+  padding: 5rem calc((100vw - 1300px) / 2);
+  background: #fff;
+  color: #fff;
+`
+
+const TripsHeading = styled.div`
+  font-size: clamp(1.8rem, 6vw, 4rem);
+  text-align: center;
+  margin-bottom: 5rem;
+  font-family: "PT Sans Caption", sans-serif;
+  font-weight: bold;
+  color: #000;
+`
+
+const TripsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 10px;
+  justify-items: center;
+  padding: 0 2rem;
+  max-width: 100%;
+  height: 100%;
+
+  @media screen and (max-width: 1200px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media screen and (max-width: 868px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+const TripCard = styled.div`
+  line-height: 2;
+  width: 100%;
+  height: 500px;
+  position: relative;
+  border-radius: 10px;
+  transition: 0.3s ease;
+
+  &:hover {
+    transform: scale(0.95);
+    transition: 0.2s ease-in;
+  }
+`
+
+const TripImg = styled(Img)`
+  height: 100%;
+  max-width: 100%;
+  position: relative;
+  border-radius: 10px;
+  filter: brightness(70%);
+  transition: 0.4s cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  &:hover {
+    filter: brightness(100%);
+    transition: 0.2s ease-in;
+  }
+`
+
+const TripInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0 2rem;
+  font-weight: bold;
+
+  @media screen and (max-width: 280px) {
+    padding: 0 2rem;
+  }
+`
+
+const TextWrap = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: 375px;
+`
+
+const TripTitle = styled.div`
+  font-weight: 400;
+  font-size: 1rem;
+  margin-left: 0.5rem;
+`
+var buttonStyle = {
+  position: "absolute",
+  top: "420px",
+  fontSize: "14px",
+}
--- src/pages/destinations.js
+++ src/pages/destinations.js
@@ -1,12 +1,13 @@
 import React from "react"
-import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Destinations from "../components/Destinations"
+import Layout from "../components/layout"
 
-const Destinations = () => (
+const Destination = () => (
   <Layout>
-    <SEO title="Page three" />
-    <h1>Destinations</h1>
+    <SEO title="Destinations" />
+    <Destinations />
   </Layout>
 )
 
-export default Destinations
+export default Destination
