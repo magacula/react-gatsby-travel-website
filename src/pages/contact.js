13446c7238bae286ad5f605bfacfe00067bd13d8
Koi <Makoi.Gacula@student.csulb.edu>
2020-12-08 15:57:08 -0800

    created contacts page

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Images = () => (
+  <Layout>
+    <SEO title="Page two" />
+    <h1>Contact</h1>
+  </Layout>
+)
+
+export default Images
